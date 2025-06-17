3cca6f2c6b5c788e25346ba3abef106269feb393
Maria Lourdes Goods <73761322+MLGoodsg@users.noreply.github.com>
2025-06-17 16:26:16 -0500

    Estableciendo conexion con mysql

--- src/base_de_datos_escolar/Main.java
+++
@@ -1,7 +0,0 @@
-package base_de_datos_escolar;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
--- src/base_de_datos_escolar/VentanaPruebaController.java
+++
@@ -1,5 +0,0 @@
-package base_de_datos_escolar;
-
-public class VentanaPruebaController {
-    // Puedes agregar métodos y elementos @FXML aquí
-}
